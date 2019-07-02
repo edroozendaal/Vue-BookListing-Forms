fcd26fefe775679aeef78081425e2d8540b36fe9
Ed Roozendaal <ed.roozendaal@INHolland.nl>
2019-07-02 22:46:24 +0200

    module1

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -3,9 +3,7 @@
 <<<<<<< HEAD
     <div>
       <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    </div>
-    <div>
-    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+      <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     </div>
     <div>
       <label for="finishedReading">
